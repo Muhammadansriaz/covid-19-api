7d4494ad45c3091a4014661da71d5b14d2bb52d3
Muhammad Ans <75665100+Muhammadansriaz@users.noreply.github.com>
2023-03-28 03:53:44 +0500

    datatype

--- api/raw/cases/county.ts
+++ api/raw/cases/county.ts
@@ -18,7 +18,7 @@ export default async (req: NowRequest, res: NowResponse) => {
       : 0;
 
   const field =
-    req.query.field in sortBy ? (req.query.field as string) : "confirmed";
+    req.query.field in sortBy ? (req.query.field as any) : "confirmed";
   const query = createSortQuery(createCountySortGroup(field), { resultOffset });
   const response = await fetcher(`${endpoint}?${qs.stringify(query)}`);
   const data = await response.json();
